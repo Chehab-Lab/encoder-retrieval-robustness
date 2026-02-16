78ef053f855b8378e2bdd01727ad6661040a8bb8
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2026-02-16 22:01:52 +0200

    set Logger

--- metric.py
+++ metric.py
@@ -1,7 +1,13 @@
 import faiss
+import os
+from logging import get_logger
+
+logger = get_logger(__name__)
+logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 
 def retrieval_mean_precision(embeddings, labels, k):
     faiss.normalize_L2(embeddings)
+    logger.debug(f"Embeddings shape: {embeddings.shape}")
     index = faiss.IndexFlatIP(embeddings.shape[1])
     index.add(embeddings)
     _, neighbors = index.search(embeddings, k + 1)
