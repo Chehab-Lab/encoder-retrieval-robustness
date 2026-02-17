9ae22bb56087f83700bfcf9934801d631a543d25
GauravPatil8 <patilgauravpradeep@gmail.com>
2026-02-17 22:51:54 +0530

    update transform function

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -26,18 +26,17 @@ def _is_already_evaluated(checkpoint_file_path, encoder_name, dataset_name):
                 return True
     return False
 
-def _apply_transform(image, transformation):
-    img = np.asarray(image)
-    img = img / 255.0
-    augmented_img = (np.array(transformation([img])) * 255).astype(np.uint8)
-    return augmented_img
+def _apply_transform(batch_images, transformation):
+    imgs = [(np.asarray(img, dtype=np.float32) / 255.0) for img in batch_images]
+    augmented = (np.array(transformation(imgs)) * 255).astype(np.uint8)
+    return augmented
 
 def _get_embeddings(encoder, dataset, transformation, img_processor, target_dim, device):
     embeddings = []
     all_labels = []
     for batch_images, labels in tqdm(dataset):
         if transformation:
-                batch_images = [_apply_transform(image, transformation) for image in batch_images]
+                batch_images = _apply_transform(batch_images, transformation)
         batch_images = img_processor(batch_images, return_tensors="pt")["pixel_values"].to(device)
         batch_emb = get_features(encoder, batch_images, target_dim, device)
         embeddings.append(batch_emb)
@@ -144,3 +143,4 @@ def _test_retreival_pipeline():
             "direction": 1
         }]
     evaluate_retrieval(encoder_name, dataset_name, 2048, transformation_obj, [RetrievalMetrics.MEAN_AVERAGE_PRECISION],[5], batch_size=256)
+_test_retreival_pipeline()
